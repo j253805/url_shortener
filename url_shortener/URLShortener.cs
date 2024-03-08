82a4c0f378b5619d0a792c63c7832844f4cdb1ab
j253805 <253805@usn.no>
2024-03-08 15:52:58 +0100

    Litt klargjøring

---
+++ url_shortener/URLShortener.cs
@@ -0,0 +1,14 @@
+namespace url_shortener;
+
+public class URLShortener
+{
+    private string longURL;
+    private string shortURL;
+
+    public URLShortener()
+    {
+        
+    }
+    
+    
+}
